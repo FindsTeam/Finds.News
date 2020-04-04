c691726e49205ff4345708108af82b362a7c00f4
GitStearis <egor.puisha@gmail.com>
2020-04-04 15:00:39 +0300

    Short & long digests for actual events

--- src/utils/editor.js
+++ src/utils/editor.js
@@ -1,19 +1,56 @@
-const { fromMongoDate } = require("./time");
+const messages = require("../constants/messages");
 
-const SEPARATOR = "\n`______________________________`\n\n";
+const { toFindsBriefDate, now, toFindsTime } = require("./time");
 
-const fullEventInformation = event => {
-    const date = fromMongoDate(event.start);
-    return `📆  ${ date }
+const SHORT_DESCRIPTION = 200;
+const LONG_DESCRIPTION = 400;
+const SEPARATOR = "`______________________________`\n";
+const EVENTS_SEPARATOR = `\n${ SEPARATOR }\n`;
 
-📌  [${ event.title }](${ event.links.post })
-    
-🗒  ${ event.description.slice(0, 400) }
-    
-🌍  ${ event.address } ([${ event.place }](${ event.links.place }))`;
+const buildBriefing = amount => {
+    return `\n📆  _${ toFindsBriefDate(now()) }_
+🎉  Ивентов найдено: *${ amount }*
+🤖  _Просим прощения, если ссылки будут битыми, описания неинформативными, а ивенты — отстойными._`;
 };
 
-const fullEventInformationDigest = events => events.map(event => fullEventInformation(event)).join(SEPARATOR);
+const buildAddress = event => {
+    const {
+        address,
+        place
+    } = event;
+
+    const placeString = place ? `[${ place }](${ event.links.place })` : "";
+    const placeSection = address && placeString ? `(${ placeString })` : placeString;
+    
+    return address ? `_${ address }_ ${ placeSection}` : placeSection;
+};
+
+const fullEventInformation = event => {
+    return `⏰  _${ toFindsTime(event.start) }_\n
+📌  [${ event.title }](${ event.links.post })\n
+🗒  ${ event.description.slice(0, LONG_DESCRIPTION) }...\n
+🌍  ${ buildAddress(event) }`;
+};
+
+const shortEventInformation = event => {
+    return `⏰  _${ toFindsTime(event.start) }_
+📌  [${ event.title }](${ event.links.post }) ${ event.description.slice(0, SHORT_DESCRIPTION) }...
+🌍  ${ buildAddress(event) }`;
+};
+
+const fullEventInformationDigest = events => {
+    return `${ messages.eventsToday }
+${ buildBriefing(events.length) }
+${ SEPARATOR }
+${ events.map(event => fullEventInformation(event)).join(EVENTS_SEPARATOR) }`;
+};
+
+const shortEventInformationDigest = events => {
+    return `${ messages.eventsToday }
+${ buildBriefing(events.length) }
+${ SEPARATOR }
+${ events.map(event => shortEventInformation(event)).join(EVENTS_SEPARATOR) }`;
+};
 
 module.exports.createEventsDigest = events => {
     const amount = events.length;
@@ -23,6 +60,6 @@ module.exports.createEventsDigest = events => {
     } else if (amount < 4) {
         return fullEventInformationDigest(events);
     } else {
-        return `🤖  Для тебя ничего не жалко, но я еще пока не умею обрабатывать ${ amount } ивентов за раз.`;
+        return shortEventInformationDigest(events);
     }
 };
--- src/utils/time.js
+++ src/utils/time.js
@@ -4,7 +4,10 @@ const { capitalize } = require("./text");
 moment.locale("ru");
 
 const MONGO_FORMAT = "YYYY-MM-DDTHH:mm:ss.SSSZ";
-const FINDS_FORMAT = "dddd, DD.MM.YYYY, HH:MM";
+const FINDS_DAY_FORMAT = "DD.MM.YYYY";
+const FINDS_START_TIME_FORMAT = "HH:mm";
+const FINDS_BRIEF_FORMAT = `dddd, ${ FINDS_DAY_FORMAT }`;
+const FINDS_FULL_FORMAT = `${ FINDS_BRIEF_FORMAT }, ${ FINDS_START_TIME_FORMAT }`;
 
 const SECOND = 1000;
 const MINUTE = 60 * SECOND;
@@ -13,6 +16,10 @@ const DAY = 24 * HOUR;
 
 module.exports.now = () => {
     return moment().valueOf();
+};
+
+module.exports.toMilliseconds = timestamp => {
+    return moment(timestamp).valueOf();
 };
 
 module.exports.endOfToday = () => {
@@ -27,8 +34,16 @@ module.exports.toMongoDate = time => {
     return moment(time).utc().format(MONGO_FORMAT);
 };
 
-module.exports.fromMongoDate = string => {
-    return capitalize(moment(string).local().format(FINDS_FORMAT));
+module.exports.toFindsFullDate = time => {
+    return capitalize(moment(time).local().format(FINDS_FULL_FORMAT));
+};
+
+module.exports.toFindsBriefDate = time => {
+    return capitalize(moment(time).local().format(FINDS_BRIEF_FORMAT));
+};
+
+module.exports.toFindsTime = time => {
+    return moment(time).local().format(FINDS_START_TIME_FORMAT);
 };
 
 module.exports.isTimePeriodLessThanOneDay = (from, to) => {
